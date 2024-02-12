44a655b8bea1016de196c98d08eb4600e76fe2b2
Bruno Gomes <bsgomes16@gmail.com>
2024-02-12 19:17:10 -0300

    Header and StatsCard concludeds, and hys styles was made with scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,24 @@
-import './styles.css'
+//Pra importar um modulo css, eu jogo pra dentro de uma variavel, e todos os parametros do meu css agr pode ser acessado como um objeto
+import { StatsCard } from "../StatsCard/StatsCard";
+import styles from "./styles.module.scss";
 
-export const Header: React.FC = () => { //Exportando como arrow function eu consigo agr tipar a minha funçao
+export const Header: React.FC = () => {
+  //Exportando como arrow function eu consigo agr tipar a minha funçao
   return (
-    <header className="header">
-      <div>
-        <h1>MyTodo</h1>
+    // Eu nao uso mais uma string comum para a classe, agr eu acesso o styles e uso o nome que eu usaria caso fosse uma string
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>MyTodo</h1>
 
-        <span>Bem vindo(a)</span>
-      </div>
+          <span>Bem vindo(a)</span>
+        </div>
 
-      <div>
-        {/* Aqui virá os cards */}
+        <div>
+          <StatsCard title="Total de tarefas" value={5} />
+          <StatsCard title="Tarefas Pendentes" value={4}/>
+          <StatsCard title="Tarefas Concluídas" value={1}/>
+        </div>
       </div>
     </header>
   );
---
+++ src/components/StatsCard/StatsCard.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+
+interface StatsCardProps {
+  title: string;
+  value: number;
+}
+
+//Logo após o React.FC eu abro um generic '<>', e passo o contrato(interface) dos parametros que vao ser recebidos
+export const StatsCard: React.FC<StatsCardProps> = (props) => {
+  return (
+    <article className={styles.stats_card}>
+      <h2>{props.title}</h2>
+
+      <span>{props.value}</span>
+    </article>
+  )
+}
