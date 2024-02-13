199fbff2791367874f4136c57839810b9f78e570
Bruno Gomes <bsgomes16@gmail.com>
2024-02-13 10:57:21 -0300

    new component Tasks initiated with his style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Header } from "./components/Header/Header";
-import './styles/global.css'
+import { Tasks } from "./components/Tasks/Tasks";
+import "./styles/global.css";
 
 function App() {
   return (
-    <Header/>
+    <>
+      <Header />
+
+      <Tasks />
+    </>
   );
 }
 
